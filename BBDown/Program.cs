fb06e53a26994224d1f46dc053c34061a0d6f91e
yukinotech <yukinotech@gmail.com>
2022-05-26 22:23:54 +0800

    fix(配置文件): 增加空白符处理

--- BBDown/Program.cs
+++ BBDown/Program.cs
@@ -335,7 +335,17 @@ namespace BBDown
                     var configArgs = File
                         .ReadAllLines(configPath)
                         .Where(s => !string.IsNullOrEmpty(s) && !s.StartsWith("#"))
-                        .SelectMany(s => s.Split(' ').Where(s => !string.IsNullOrEmpty(s)).Select(s => s.Trim('\"')));
+                        .SelectMany(s => {
+                                var trimLine = s.Trim();
+                                if (trimLine.IndexOf('-') == 0 && trimLine.IndexOf(' ') != -1) {  
+                                    var spaceIndex = trimLine.IndexOf(' ');
+                                    var paramsGroup = new String[] { trimLine.Substring(0,spaceIndex), trimLine.Substring(spaceIndex) };
+                                    return paramsGroup.Where(s => !string.IsNullOrEmpty(s)).Select(s => s.Trim(' ').Trim('\"'));
+                                } else {
+                                    return new String[] {trimLine.Trim('\"')};
+                                }                    
+                            }
+                        );
                     var configArgsResult = rootCommand.Parse(configArgs.ToArray());
                     foreach (var item in configArgsResult.CommandResult.Children)
                     {
