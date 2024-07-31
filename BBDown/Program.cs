9d5a88f0a2620062ecad686f046b9ad251dbe3bf
謙蘭君 <hewun@lyceum.cc>
2024-07-31 18:56:27 +0800

    fix: If it is interactive video and used tv api, then remove tv api flag

--- BBDown/Program.cs
+++ BBDown/Program.cs
@@ -264,8 +264,13 @@ namespace BBDown
             {
                 Log($"UP主页: https://space.bilibili.com/{mid}");
             }
-            string apiType = myOption.UseTvApi && !vInfo.IsSteinGate ? // 互动视频不能使用TV接口
-                "TV" : (myOption.UseAppApi ? "APP" : (myOption.UseIntlApi ? "INTL" : "WEB"));
+
+            if (vInfo.IsSteinGate && myOption.UseTvApi)
+            {
+                Log("视频为互动视频，暂时不支持tv下载，修改为默认下载");
+                myOption.UseTvApi = false;
+            }
+            string apiType = myOption.UseTvApi ? "TV" : (myOption.UseAppApi ? "APP" : (myOption.UseIntlApi ? "INTL" : "WEB"));
 
             //打印分P信息
             List<Page> pagesInfo = vInfo.PagesInfo;
