0a1a267272ba0e1cb228ff0c79326f68db624bf5
UnforgetMemory <2466026525@qq.com>
2024-08-27 17:33:53 +0800

    🎈 perf:

--- BBDown.Core/Parser.cs
+++ BBDown.Core/Parser.cs
@@ -48,7 +48,7 @@ namespace BBDown.Core
             {
                 // 尝试提高可读性
                 StringBuilder apiBuilder = new();
-                apiBuilder.Append($"support_multi_audio=true&avid={aid}&cid={cid}&fnval=4048&fnver=0&fourk=1");
+                apiBuilder.Append($"support_multi_audio=true&from_client=BROWSER&avid={aid}&cid={cid}&fnval=4048&fnver=0&fourk=1");
                 if (Config.AREA != "") apiBuilder.Append($"&access_key={Config.TOKEN}&area={Config.AREA}");
                 apiBuilder.Append($"&otype=json&qn={qn}");
                 if (bangumi) apiBuilder.Append($"&module=bangumi&ep_id={epId}&session=");
